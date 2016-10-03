c81b419667cabaef3f45648a885a786ecc6a1d27
Andrew Teixeira <teixeira@broadinstitute.org>
2016-10-03 10:42:40 -0400

    Add hiera requirement to spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'puppetlabs_spec_helper/module_spec_helper'
 require 'rspec-puppet-facts'
+require 'hiera'
 
 # Uncomment this to show coverage report, also useful for debugging
 #at_exit { RSpec::Puppet::Coverage.report! }
